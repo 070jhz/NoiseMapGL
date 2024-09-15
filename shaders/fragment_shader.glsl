6fe54d73db981f67e15c6e479ebdc7119e53c20d
abksm <belkasmiachraf070@gmail.com>
2024-09-15 22:59:46 +0200

    two more if statements lol

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -8,24 +8,27 @@ void main()
 	vec3 color;
 
 	if (height < 10.0)
-		color = mix(vec3(0.0, 0.0, 1.0), vec3(0.0, 0.5, 0.0), height / 10.0); // Blue to Dark Green
+	color = mix(vec3(0.0, 0.0, 1.0), vec3(0.0, 0.5, 0.0), height / 10.0);
 	else if (height < 20.0)
-		color = mix(vec3(0.0, 0.5, 0.0), vec3(0.0, 0.7, 0.0), (height - 10.0) / 10.0); // Dark Green to Green
+	color = mix(vec3(0.0, 0.5, 0.0), vec3(0.0, 0.7, 0.0), (height - 10.0) / 10.0);
 	else if (height < 30.0)
-		color = mix(vec3(0.0, 0.7, 0.0), vec3(0.5, 0.5, 0.0), (height - 20.0) / 10.0); // Green to Olive
+	color = mix(vec3(0.0, 0.7, 0.0), vec3(0.5, 0.5, 0.0), (height - 20.0) / 10.0);
 	else if (height < 40.0)
-		color = mix(vec3(0.5, 0.5, 0.0), vec3(0.7, 0.7, 0.0), (height - 30.0) / 10.0); // Olive to Light Olive
+	color = mix(vec3(0.5, 0.5, 0.0), vec3(0.7, 0.7, 0.0), (height - 30.0) / 10.0);
 	else if (height < 50.0)
-		color = mix(vec3(0.7, 0.7, 0.0), vec3(0.8, 0.5, 0.0), (height - 40.0) / 10.0); // Light Olive to Brown
+	color = mix(vec3(0.7, 0.7, 0.0), vec3(0.8, 0.5, 0.0), (height - 40.0) / 10.0);
 	else if (height < 60.0)
-		color = mix(vec3(0.8, 0.5, 0.0), vec3(0.5, 0.3, 0.0), (height - 50.0) / 10.0); // Brown to Dark Brown
-	else if(height < 70.0)
-		color = mix(vec3(0.5, 0.3, 0.0), vec3(0.6, 0.4, 0.4), (height - 60.0) / 10.0); // Dark Brown to Light Brown
+	color = mix(vec3(0.8, 0.5, 0.0), vec3(0.5, 0.3, 0.0), (height - 50.0) / 10.0);
+	else if (height < 70.0)
+	color = mix(vec3(0.5, 0.3, 0.0), vec3(0.6, 0.4, 0.4), (height - 60.0) / 10.0);
 	else if (height < 80.0)
-		color = mix(vec3(0.6, 0.4, 0.4), vec3(0.7, 0.7, 0.7), (height - 70.0) / 10.0); // Light Brown to Light Gray
+	color = mix(vec3(0.6, 0.4, 0.4), vec3(0.7, 0.7, 0.7), (height - 70.0) / 10.0);
+	else if (height < 90.0)
+	color = mix(vec3(0.7, 0.7, 0.7), vec3(0.8, 0.8, 0.8), (height - 80.0) / 10.0);
+	else if (height < 100.0)
+	color = mix(vec3(0.8, 0.8, 0.8), vec3(0.9, 0.9, 0.9), (height - 90.0) / 10.0);
 	else
-		color = vec3(0.5, 0.5, 0.5); // Gray for mountain peaks
-
+	color = vec3(0.9, 0.9, 0.9);
 
 	FragColor = vec4(color, 1.0);
 }
